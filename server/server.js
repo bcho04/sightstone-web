adf6ce09a08b9a229693d7e79875b50e07898e34
bcho04 <brandon.cho@gmail.com>
2020-01-17 16:28:24 -0800

    Add riot.txt

--- server/server.js
+++ server/server.js
@@ -4,12 +4,18 @@ const process = require("process");
 
 const app = express();
 const distPath = path.join(__dirname, '..', 'dist');
+const serverPath = path.join(__dirname);
 const port = process.env.PORT || 3000;
 
 app.use(express.static(distPath));
 app.get('/', (req, res) => {
    res.sendFile(path.join(distPath, 'index.html'));
 });
+
+app.get('/riot.txt', (req, res) => {
+   res.sendFile(path.join(serverPath, 'riot.txt'));
+});
+
 app.listen(port, () => {
    console.log('Server is up.');
 });
